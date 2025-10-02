97c24f5baec4692b980a391482141e8d0b06dfd5
cuorain <cuore623.prog@gmail.com>
2025-10-02 08:27:15 +0000

    環境変数からエンドポイント取得する

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,12 +1,15 @@
 class AuthService {
   async login(username, password) {
-    const response = await fetch("/api/v1/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      }
+    );
     const data = await response.json();
     if (response.ok) {
       localStorage.setItem("token", data.token);
@@ -17,13 +20,16 @@ class AuthService {
   }
 
   async register(username, password) {
-    const response = await fetch("/api/v1/auth/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username, password }),
-    });
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/v1/auth/register`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      }
+    );
     const data = await response.json();
     if (response.ok) {
       localStorage.setItem("token", data.token);
