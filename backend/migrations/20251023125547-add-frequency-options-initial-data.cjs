f7e4df05502548da8534dd4ec5a37eac0fb4c99a
cuorain <cuore623.prog@gmail.com>
2025-10-23 12:59:29 +0000

    fix: 目標頻度のオプションをDBに設定

---
+++ backend/migrations/20251023125547-add-frequency-options-initial-data.cjs
@@ -0,0 +1,28 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "FrequencyOptions",
+      [
+        { name: "毎日", created_at: new Date(), updated_at: new Date() },
+        { name: "週に1回", created_at: new Date(), updated_at: new Date() },
+        { name: "週に2回", created_at: new Date(), updated_at: new Date() },
+        { name: "週に3回", created_at: new Date(), updated_at: new Date() },
+        { name: "週に4回", created_at: new Date(), updated_at: new Date() },
+        { name: "週に5回", created_at: new Date(), updated_at: new Date() },
+        { name: "週に6回", created_at: new Date(), updated_at: new Date() },
+        { name: "毎週", created_at: new Date(), updated_at: new Date() },
+        { name: "隔週", created_at: new Date(), updated_at: new Date() },
+        { name: "毎月", created_at: new Date(), updated_at: new Date() },
+        { name: "毎年", created_at: new Date(), updated_at: new Date() },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("FrequencyOptions", null, {});
+  },
+};
